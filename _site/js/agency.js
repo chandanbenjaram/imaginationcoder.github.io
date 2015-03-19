50c76da2e48d70454a4fe0d53696f147680b04c1
imaginationcoder <labs@maisasolutions.com>
2015-03-19 12:00:30 +0530

    imported blog from other repo

---
+++ _site/js/agency.js
@@ -0,0 +1,64 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+$('div.modal').on('show.bs.modal', function() {
+	var modal = this;
+	var hash = modal.id;
+	window.location.hash = hash;
+	window.onhashchange = function() {
+		if (!location.hash){
+			$(modal).modal('hide');
+		}
+	}
+});
+
+// --- Added by Upender -------------------------
+// to support individual pages SEO
+var anchor = ''
+switch (window.location.pathname) {
+    case '/services/':
+        anchor = $('a[href="#services"]')
+        break;
+    case '/process/':
+        anchor = $('a[href="#process"]')
+        break;
+    case '/portfolio/':
+        anchor = $('a[href="#portfolio"]')
+        break;
+    case '/pride/':
+        anchor = $('a[href="#our-pride"]')
+        break;
+    case '/contact/':
+        anchor = $('a[href="#contact"]')
+        break;
+}
+if(anchor !='') {
+    $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+    }, 1000, 'easeInOutExpo');
+}
+// -------------------------------------------------
